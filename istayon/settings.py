61a58f62338a7cb419f94230a96038fe501eca34
Austin Valeske <avaleske@gmail.com>
2014-12-13 01:10:59 -0800

    adding memcached things

--- istayon/settings.py
+++ istayon/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import dj_database_url
+import urlparse
+import json
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -48,6 +50,17 @@ TEMPLATE_LOADERS = (
         'django.template.loaders.app_directories.Loader',
     )),
 )
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_bmemcached.memcached.BMemcached',
+        'LOCATION': os.environ.get('MEMCACHEDCLOUD_SERVERS').split(','),
+        'OPTIONS': {
+                    'username': os.environ.get('MEMCACHEDCLOUD_USERNAME'),
+                    'password': os.environ.get('MEMCACHEDCLOUD_PASSWORD')
+            }
+    }
+}
 
 ROOT_URLCONF = 'istayon.urls'
 
